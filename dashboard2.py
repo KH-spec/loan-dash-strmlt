deeb59fd10680bba7fa7bb510a9fd117d981ddd9
KH-spec <81872937+KH-spec@users.noreply.github.com>
2022-12-03 00:47:30 +0100

    Update dashboard2.py

--- dashboard2.py
+++ dashboard2.py
@@ -23,7 +23,8 @@ def main():
     # --------------------------------------------------------------------------------------------------
    
     #MLFLOW_URI = 'http://192.168.1.86:8501'
-    API_URL = "http://127.0.0.1:5000/app/"
+    #API_URL = "http://127.0.0.1:5000/app/"
+    API_URL = "https://kh-spec-loan-dashboard-using-streamlit-on-her-dashboard2-49sb10.streamlit.app/"
     
     #api_choice = st.sidebar.selectbox('Quelle API souhaitez vous utiliser',['','MLflow'])
     #st.set_page_config(page_icon='ðŸ§Š',layout='centered',initial_sidebar_state='auto')
