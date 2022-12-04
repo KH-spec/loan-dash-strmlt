fbe3c193d5d2523c5a6af8778be33dab9c73c2ee
KH-spec <81872937+KH-spec@users.noreply.github.com>
2022-12-04 16:52:17 +0100

    Update dashboard2.py

--- dashboard2.py
+++ dashboard2.py
@@ -47,7 +47,19 @@ def main():
     #st.image(img, width=200)
     with col1:
         st.image(img, width=700)
-    
+    st.session_state['answer'] = ''!
+
+    st.write(st.session_state)
+
+    realans = ['', 'abc', 'edf']
+
+    if  st.session_state['answer'] in realans:
+        answerStat = "correct"
+    elif st.session_state['answer'] not in realans:
+        answerStat = "incorrect"
+
+    st.write(st.session_state)
+    st.write(answerStat)
     # --------------------------------------------------------------------------------------------------
     #                          list of ids customers
     # --------------------------------------------------------------------------------------------------
